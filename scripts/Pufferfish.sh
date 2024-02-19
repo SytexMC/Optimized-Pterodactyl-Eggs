c17493d278305d8320019cf7a1c0619b854e79a1
SytexMC <123174767+SytexMC@users.noreply.github.com>
2024-02-19 12:36:03 +0100

    Update Pufferfish.sh

--- scripts/Pufferfish.sh
+++ scripts/Pufferfish.sh
@@ -1,62 +1,60 @@
 #!/bin/ash
 # Pufferfish Installation Script
 # Server Files: /mnt/server
-PROJECT=pufferfish
 
 if [ -n "$DL_PATH" ]; then
-echo "Using supplied download url: $DL_PATH"
-DOWNLOAD_URL=$(eval echo $(echo "$DL_PATH" | sed -e 's/{{/${/g' -e 's/}}/}/g'))
+    echo "Using supplied download url: $DL_PATH"
+    DOWNLOAD_URL=$(eval echo $(echo "$DL_PATH" | sed -e 's/{{/${/g' -e 's/}}/}/g'))
 else
     
-# quick fix with wanting to use 1.20.1
-if [ "$MINECRAFT_VERSION" == "1.20.1" ]; then
-BUILD_NUMBER="27"
-fi
+    # Quick fix with wanting to use 1.20.1
+    if [ "$MINECRAFT_VERSION" == "1.20.1" ]; then
+        BUILD_NUMBER="27"
+    fi
 
-# Pufferfish doesn't use the patch version of semver, so if we end up given one as a response, we remove it
-if echo "$MINECRAFT_VERSION" | grep -qE '^[0-9]+\.[0-9]+\.[0-9]+$'; then
-# Remove the third component from semver
-MINECRAFT_VERSION=$(echo "$MINECRAFT_VERSION" | awk -F. '{print $1"."$2}')
-fi
-# Queries the CI so we can figure out what versions of pufferfish is available. I'm also filtering out pufferfish+ and pufferfish purpur as they aren't needed
-VERSIONS=$(curl -s "https://ci.pufferfish.host/api/json" | jq -r '.jobs[].name | select(test("Pufferfishplus|Pufferfish-Purpur"; "i") | not)')
-# We check minecraft version given against the available puffferfish versions via regex.
-VERSION=""
-for v in $VERSIONS; do
-if echo "$v" | grep -qE ".*$MINECRAFT_VERSION.*"; then
-VERSION="$v"
-break
-fi
+    if echo "$MINECRAFT_VERSION" | grep -qE '^[0-9]+\.[0-9]+\.[0-9]+$'; then
+        MINECRAFT_VERSION=$(echo "$MINECRAFT_VERSION" | awk -F. '{print $1"."$2}')
+    fi
+
+    VERSIONS=$(curl -s "https://ci.pufferfish.host/api/json" | jq -r '.jobs[].name | select(test("Pufferfishplus|Pufferfish-Purpur"; "i") | not)')
+    VERSION=""
+    for v in $VERSIONS; do
+
+    if echo "$v" | grep -qE ".*$MINECRAFT_VERSION.*"; then
+        VERSION="$v"
+        break
+    fi
+
 done
-# Verify if it exists + get latest version
-VER_EXISTS=$([ "$VERSION" != "" ] && echo true || echo false)
-LATEST_VERSION=$(echo "$VERSIONS" | tr ' ' '\n' | sort -rV | head -n 1)
-# if not valid switch to latest. This is important as we avoid errors from querying invalid versions
-if [ "$VER_EXISTS" = "true" ]; then
-echo "Version is valid. Using version $MINECRAFT_VERSION"
-else
-echo "Specified version not found. Defaulting to the latest Pufferfish version"
-MINECRAFT_VERSION="$LATEST_VERSION"
-VERSION="$LATEST_VERSION"
-fi
-# checks if build is valid and gets latest build
-BUILD_EXISTS=$(curl -s "https://ci.pufferfish.host/job/$VERSION/api/json/" | jq -r --arg BUILD "$BUILD_NUMBER" '.builds[].number | tostring | contains($BUILD)' | grep -m1 true)
-LATEST_BUILD=$(curl -s "https://ci.pufferfish.host/job/$VERSION/api/json" | jq -r '.builds[0].number')
-if [ "$BUILD_EXISTS" = "true" ]; then
-echo "Build is valid for version $MINECRAFT_VERSION. Using build $BUILD_NUMBER"
-else
-echo "Using the latest Pufferfish build for version $MINECRAFT_VERSION"
-BUILD_NUMBER="$LATEST_BUILD"
-fi
-# grabbing the jar's name. Needed to download the artifact
-JAR_NAME=$(curl -s "https://ci.pufferfish.host/job/$VERSION/$BUILD_NUMBER/api/json/" | jq -r '.artifacts[0].fileName')
+    VER_EXISTS=$([ "$VERSION" != "" ] && echo true || echo false)
+    LATEST_VERSION=$(echo "$VERSIONS" | tr ' ' '\n' | sort -rV | head -n 1)
+    
+    if [ "$VER_EXISTS" = "true" ]; then
+        echo "Version is valid. Using version $MINECRAFT_VERSION"
+    else
+        echo "Specified version not found. Defaulting to the latest Pufferfish version"
+        MINECRAFT_VERSION="$LATEST_VERSION"
+        VERSION="$LATEST_VERSION"
+    fi
 
-echo -e "Version being downloaded"
-echo -e "MC Version: $MINECRAFT_VERSION"
-echo -e "Build: $BUILD_NUMBER"
-echo -e "JAR Name of Build: $JAR_NAME"
-DOWNLOAD_URL="https://ci.pufferfish.host/job/$VERSION/$BUILD_NUMBER/artifact/build/libs/$JAR_NAME"
-if
+    BUILD_EXISTS=$(curl -s "https://ci.pufferfish.host/job/$VERSION/api/json/" | jq -r --arg BUILD "$BUILD_NUMBER" '.builds[].number | tostring | contains($BUILD)' | grep -m1 true)
+    LATEST_BUILD=$(curl -s "https://ci.pufferfish.host/job/$VERSION/api/json" | jq -r '.builds[0].number')
+
+    if [ "$BUILD_EXISTS" = "true" ]; then
+        echo "Build is valid for version $MINECRAFT_VERSION. Using build $BUILD_NUMBER"
+    else
+        echo "Using the latest Pufferfish build for version $MINECRAFT_VERSION"
+        BUILD_NUMBER="$LATEST_BUILD"
+    fi
+
+    JAR_NAME=$(curl -s "https://ci.pufferfish.host/job/$VERSION/$BUILD_NUMBER/api/json/" | jq -r '.artifacts[0].fileName')
+
+    echo -e "Version being downloaded"
+    echo -e "MC Version: $MINECRAFT_VERSION"
+    echo -e "Build: $BUILD_NUMBER"
+    echo -e "JAR Name of Build: $JAR_NAME"
+    DOWNLOAD_URL="https://ci.pufferfish.host/job/$VERSION/$BUILD_NUMBER/artifact/build/libs/$JAR_NAME"
+fi
 
 cd /mnt/server
 
@@ -67,7 +65,6 @@ if [ -f ${SERVER_JARFILE} ]; then
 fi
 
 curl -o ${SERVER_JARFILE} ${DOWNLOAD_URL}
-
 
 # server-icon.png
 
@@ -81,7 +78,6 @@ fi
 if [ ! -f server.properties ]; then
 	echo -e "Downloading server.properties"
     curl -o server.properties https://raw.githubusercontent.com/sytexmc/optimized-pterodactyl-eggs/main/configs/server.properties
-
 fi
 
 # bukkit.yml
